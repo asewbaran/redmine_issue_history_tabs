8d31a3d4eca281c9f16f199e9405969eac1e381d
Ashwin Sewbaran <Ashwin.Sewbaran@gmail.com>
2011-10-25 12:10:11 +0200

    Version 0.1.1

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_issue_history_tabs do
   name 'Redmine Issue History Tabs plugin'
-  author 'Konstantin Zaitsev'
-  description 'This plugin provide history of issue in tabs representation'
-  version '0.0.2'
+  author 'Ashwin Sewbaran'
+  description 'This plugin provide history of issue in tabs representation (Original by Konstantin Zaitsev)'
+  version '0.1.1'
 end
